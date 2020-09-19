4a5ccf4fb8191b737c19bec6297cd01db1980e0a
iman_syed <iman.syed101@gmail.com>
2020-09-19 20:29:04 +0100

    Initial commit - still need to write readme

---
+++ test/bctest.py
@@ -0,0 +1,27 @@
+# load text
+itemsfile='test\\testitems.txt'
+blocklistfile = 'test\\blocklist.txt'
+
+# write text files to lists and clean up for search
+with open(blocklistfile, "r") as i:
+    blockedlines = [word.strip() for word in i.readlines()]
+with open(itemsfile, "r") as i:
+    itemlines = [word.strip() for word in i.readlines()]
+    itemlines = [x.lower() for x in itemlines]
+
+
+# determine lines with blocked words in and make list of non-blocked lines
+# using break to avoid e.g. 'i had a bad bad day' appearing twice in the list
+cleanlines=[]
+for sentence in itemlines:
+    bad_sentence = False
+    for blockedword in blockedlines:
+        if blockedword in sentence:
+            bad_sentence = True
+            break
+    if not bad_sentence:
+        cleanlines.append(sentence)    
+
+# change list to text file
+with open('cleanitems.txt', 'w') as filehandle:
+    filehandle.writelines("%s\n" % lines for lines in cleanlines)
